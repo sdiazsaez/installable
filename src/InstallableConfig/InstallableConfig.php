945e0325390a1e35d6d7b6ebec9237f94cae3c82
simon <sdiaz.sz@gmail.com>
2024-11-21 23:06:39 -0300

    add debug

--- src/InstallableConfig/InstallableConfig.php
+++ src/InstallableConfig/InstallableConfig.php
@@ -7,6 +7,23 @@ class InstallableConfig {
     public function config(string $key): InstallableEntryConfig {
         $configPath = 'installable.migrations.' . $key;
         $attributes = config($configPath);
+
+        if(!is_array($attributes)) {
+            dump([
+                $key,
+                $configPath,
+                $attributes,
+                //debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 3),
+            ]);
+            $trace = debug_backtrace();
+            foreach ($trace as $frame) {
+                if (isset($frame['function'])) {
+                    echo $frame['function'] . '() called at ' . $frame['file'] . ':' . $frame['line'] . "\n";
+                }
+            }
+
+        }
+
         return new InstallableEntryConfig($configPath, $attributes);
     }
 
